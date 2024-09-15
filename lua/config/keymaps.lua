e64da5ca8b41fb87e20322b1f21e0d7701ad28bf
d2h577yf <why20051028XY@icloud.com>
2024-09-15 10:23:19 +0800

    First
    这是可以用来学习的第一个版本

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,28 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+------------keymap
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+-- ---------- 插入模式 ---------- ---
+keymap.set("i", "ii", "<ESC>")
+
+-- ---------- 视觉模式 ---------- ---
+-- 单行或多行移动
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- ---------- 正常模式 ---------- ---
+-- 窗口
+keymap.set("n", "<leader>sv", "<C-w>v") -- 水平新增窗口
+keymap.set("n", "<leader>sh", "<C-w>s") -- 垂直新增窗口
+
+-- 取消高亮
+keymap.set("n", "<leader>nh", ":nohl<CR>")
+
+-- 切换buffer
+keymap.set("n", "<C-A>", ":bnext<CR>")
+keymap.set("n", "<C-D>", ":bprevious<CR>")
---
+++ lua/config/options.lua
@@ -0,0 +1,11 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.cmd([[hi Normal guibg=NONE ctermbg=NONE]])
+vim.cmd([[hi NonText guibg=NONE ctermbg=NONE]])
+vim.cmd([[hi SignColumn guibg=NONE ctermbg=NONE]])
+vim.cmd([[hi StatusLine guibg=NONE ctermbg=NONE]])
+vim.cmd([[hi StatusLineNC guibg=NONE ctermbg=NONE]])
+vim.cmd([[hi VertSplit guibg=NONE ctermbg=NONE]])
+
+local opt = vim.opt
